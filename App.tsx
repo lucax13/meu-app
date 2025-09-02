938baf6056e9c4358c42c0b62b7eaa2b94e50090
lucax13 <lucas13lima2006@gmail.com>
2025-09-02 11:26:28 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -13,12 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#9d00e6",
+    backgroundColor: "#e68200",
     alignItems: "center",
     justifyContent: "center",
   },
 
   texto: {
-    fontSize: 22,
+    fontSize: 20,
   },
 });
